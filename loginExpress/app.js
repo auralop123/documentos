707e918158183e778715ca4c1cf5d751649340c7
Auralop123 <auraaponte17@gmail.com>
2024-10-13 11:25:57 -0500

    Proyecto backend de inicio de session

--- loginExpress/app.js
+++ loginExpress/app.js
@@ -5,6 +5,8 @@ const port = 3000
 const mysql = require('mysql2/promise');
 const cors = require('cors')
 const session = require('express-session')
+const md5 = require('md5');
+
 app.use(cors({
     origin: 'http://localhost:5173',
         credentials: true
@@ -29,9 +31,10 @@ app.get('/login', async (req, res) => { //req = request, peticion; res = respons
     // A simple SELECT query
     try {
         const [results, fields] = await connection.query(
-   "SELECT * FROM `usuarios` WHERE `usuario` = ? AND`clave` = ?",
-        [datos.usuario, datos.clave]
+   "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
+        [datos.usuario, md5(datos.clave)]
  );
+ console.log(md5(datos.clave))
 if (results.length > 0) {
     req.session.usuario = datos.usuario;
     res.status(200).send('Inicio de sesión correcto')
@@ -57,12 +60,16 @@ app.get('/validar', (req, res) => {
 
 
 app.get('/registro', async (req, res) => { //req = request, peticion; res = response, respuesta
+    if (!req.session.usuario) {
+        res.status(401).send ('No autorizado')
+        return
+    } 
     const datos = req.query;
     // A simple SELECT query
     try {
         const [results, fields] = await connection.query(
    "INSERT INTO `usuarios` (`id`, `usuario`, `clave`) VALUES (NULL, ?, ?);",
-        [datos.usuario, datos.clave]
+        [datos.usuario, md5(datos.clave)]
  );
 if (results.affectedRows > 0) {
     req.session.usuario = datos.usuario;
@@ -77,8 +84,54 @@ console.log(fields); // fields contains extra meta data about results, if availa
     console.log(err);
     res.status(500).send('Error en el servidor')
 }
-
 })
+
+
+app.get('/usuarios', async (req, res) => { //req = request, peticion; res = response, respuesta
+    if (!req.session.usuario) {
+        res.status(401).send ('No autorizado')
+        return
+    } // A simple SELECT query
+    try {
+        const [results, fields] = await connection.query(
+   "SELECT * FROM `usuarios`"
+ );
+res.status(200).json(results)
+
+console.log(results); // results contains rows returned by server
+console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+    console.log(err);
+    res.status(500).send('Error en el servidor')
+}
+})
+
+app.delete('/usuarios', async (req, res) => { //req = request, peticion; res = response, respuesta
+    if (!req.session.usuario) {
+        res.status(401).send ('No autorizado')
+        return
+    }
+    const datos = req.query;
+     // A simple SELECT query
+    try {
+        const [results, fields] = await connection.query(
+   "DELETE FROM usuarios WHERE `usuarios`.`id` = ?",
+   [datos.id]
+ );
+ if (results.affectedRows > 0) {
+    res.status(200).send('Usuario eliminado')
+} else {
+    res.status(401).send('No se pudo eliminar')
+}
+
+console.log(results); // results contains rows returned by server
+console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+    console.log(err);
+    res.status(500).send('Error en el servidor')
+}
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +10,7 @@ function App() {
   const [logueado, setLogueado] = useState(false)
   const [usuarioRegistro, setUsuarioRegistro] = useState('')
   const [claveRegistro, setClaveRegistro] = useState('')
+  const [usuarios, setUsuarios] = useState([])
 
 
 
@@ -31,19 +32,21 @@ function App() {
 
 
   async function ingresar() {
-    const peticion = await fetch ('http://localhost:3000/login?usuario=' + usuario + '&clave' + clave, {credentials: 'include' })
-  if (peticion.ok) {
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave' + clave, { credentials: 'include' })
+    if (peticion.ok) {
       setLogueado(true)
+      obtenerUsuarios();
     } else {
       alert('Usuario o clave incorrectos')
     }
   }
 
   async function registrar() {
-    const peticion = await fetch('http://localhost:3000/registro?usuario=' + usuarioRegistro + '&clave' + claveRegistro, {credentials: 'include' })
-  if (peticion.ok) {
-    alert("Usuario registrado")
+    const peticion = await fetch('http://localhost:3000/registro?usuario=' + usuarioRegistro + '&clave' + claveRegistro, { credentials: 'include' })
+    if (peticion.ok) {
+      alert("Usuario registrado")
       setLogueado(true)
+      obtenerUsuarios();
     } else {
       alert('Usuario no registrado')
     }
@@ -51,32 +54,82 @@ function App() {
 
 
   async function validar() {
-    const peticion = await fetch('http://localhost:3000/validar', {credentials: 'include' })
-  if (peticion.ok) {
+    const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
+    if (peticion.ok) {
       setLogueado(true)
+      obtenerUsuarios();
+    }
+  }
+
+  async function obtenerUsuarios() {
+    const peticion = await fetch('http://localhost:3000/usuarios', { credentials: 'include' })
+    if (peticion.ok) {
+      const respuesta = await peticion.json()
+      setUsuarios(respuesta)
+    }
+  }
+  
+  async function eliminarUsuario(id) {
+    const peticion = await fetch('http://localhost:3000/usuarios?id=' + id, { credentials: 'include', method: 'DELETE' })
+    if (peticion.ok) {
+      alert('Usuario eliminado')
+      setUsuarios(respuesta)
+      obtenerUsuarios();
     }
   }
 
   useEffect(() => {
     validar()
+    
   }, [])
 
 
   if (logueado) {
-    return <Conversor />
+    return (
+      <>
+    <Conversor />
+    <h1>Registro</h1>
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuarioRegistro} onChange={cambiarUsuarioRegistro} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value={claveRegistro} onChange={cambiarClaveRegistro} />
+      <button onClick={registrar}>Registrar</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Usuario</th>
+            <th>Clave</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            usuarios.map(usuario => (
+              <tr key={usuario.id}>
+                <th>{usuario.id}</th>
+                <th>{usuario.usuario}</th>
+                <th>{usuario.clave}</th>
+                <th>
+                  <button
+                  onClick={()=>{eliminarUsuario(usuario.id)}}
+                  >X</button>
+                </th>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>)
   }
 
   return (
     <>
       <h1>Inicio de sesion</h1>
-      <input placeholder='Usuario'  type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
-      <input placeholder='Clave'  type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
+      <input placeholder='Usuario' type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario} />
+      <input placeholder='Clave' type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
       <button onClick={ingresar}>Ingresar</button>
 
-      <h1>Registro</h1>
-      <input placeholder='Usuario'  type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuarioRegistro} />
-      <input placeholder='Usuario'  type="password" name="clave" id="clave" value={clave} onChange={cambiarClaveRegistro} />
-      <button onClick={registrar}>Registrar</button>
+
 
     </>
   )
